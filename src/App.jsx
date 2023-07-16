a2a2a700ed70c6b3c563ec6977789cd52aa65e96
Kristina Acheampong <kri.acheampong@gmail.com>
2023-07-16 12:10:05 -0400

    modifying

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react';
+import { puppyList } from './data';
 import './App.css'
 
 function App() {
-  
-
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log("puppyList: ", puppyList);
   return (
-    <div className="">
-
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
     </div>
   );
 }
